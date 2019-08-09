b5efc27cbf3471f2f2f02991255ab13e96615703
Brenden Coleman <bcole2433@gmail.com>
2019-08-09 13:01:26 -0400

    set up loading featured rooms

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,16 +2,19 @@ import React from 'react'
 import Hero from '../components/Hero';
 import Banner from '../components/Banner';
 import { Link } from 'react-router-dom';
+import FeaturedRooms from '../components/FeaturedRooms';
 
 
 const Home = () => {
     return (
+        <>
         <Hero>
         <Banner title="luxurious rooms" subtitle="deluxe rooms starting at $200">
         <Link to="/rooms" className="btn-primary">Our Rooms</Link>
-
         </Banner>
         </Hero>
+        <FeaturedRooms />
+        </>
     )
 }
 
