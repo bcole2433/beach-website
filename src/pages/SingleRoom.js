543a31ce36a0b3d65fb3923b5566121e7eb85aa3
Brenden Coleman <bcole2433@gmail.com>
2019-08-09 16:19:16 -0400

    Committing before working on the Single Room component

--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -1,4 +1,9 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Hero from '../components/Hero';
+import Banner from '../components/Banner';
+import { Link } from 'react-router-dom';
+import { RoomContext } from '../Context';
+
 
 export default class SingleRoom extends Component {
     render() {
