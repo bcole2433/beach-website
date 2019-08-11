6ea02ed18375848ada0c730db6c2448cf717cede
Brenden Coleman <bcole2433@gmail.com>
2019-08-11 10:04:42 -0400

    fix for checkbox bug in handleChange

--- src/Context.js
+++ src/Context.js
@@ -59,7 +59,7 @@ class RoomProvider extends Component {
 
   handleChange = e => {
     const target = e.target;
-    const value = e.type === "checkbox" ? target.checked : target.value;
+    const value = target.type === "checkbox" ? target.checked : target.value;
     const name = e.target.name;
     this.setState(
       {
@@ -86,6 +86,7 @@ class RoomProvider extends Component {
 
     //converting values
     capacity = parseInt(capacity);
+    price = parseInt(price);
 
     //filter by type
     if (type !== "all") {
@@ -99,6 +100,19 @@ class RoomProvider extends Component {
 
     //filter price
     tempRooms = tempRooms.filter(room => room.price <= price);
+
+    //filter by size
+    tempRooms = tempRooms.filter(
+      room => room.size >= minSize && room.size <= maxSize
+    );
+    //filter by breakfast
+    if (breakfast) {
+      tempRooms = tempRooms.filter(room => room.breakfast === true);
+    }
+    //filter by pets
+    if (pets) {
+      tempRooms = tempRooms.filter(room => room.pets === true);
+    }
 
     this.setState({
       sortedRooms: tempRooms
--- src/components/RoomFilter.js
+++ src/components/RoomFilter.js
@@ -93,6 +93,55 @@ const RoomFilter = ({ rooms }) => {
           />
         </div>
         {/*end room price */}
+
+        {/* size */}
+        <div className="form-group">
+          <label htmlFor="price">room size </label>
+          <div className="size-inputs">
+            <input
+              type="number"
+              name="minSize"
+              value={minSize}
+              onChange={handleChange}
+              className="size-input"
+            />
+            <input
+              type="number"
+              name="maxSize"
+              value={maxSize}
+              onChange={handleChange}
+              className="size-input"
+            />
+          </div>
+        </div>
+        {/* end of size type */}
+
+        {/* breakfast  */}
+        <div className="form-group">
+          <div className="single-extra">
+            <input
+              type="checkbox"
+              name="breakfast"
+              id="breakfast"
+              checked={breakfast}
+              onChange={handleChange}
+            />
+            <label htmlFor="breakfast">breakfast</label>
+          </div>
+          {/* end of breakfast */}
+
+          {/* pets  */}
+          <div className="single-extra">
+            <input
+              type="checkbox"
+              name="pets"
+              checked={pets}
+              onChange={handleChange}
+            />
+            <label htmlFor="breakfast">pets</label>
+          </div>
+        </div>
+        {/* end of pets */}
       </form>
     </section>
   );
