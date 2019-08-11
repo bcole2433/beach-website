f6756abd11c504693a6c04168b1f781a2385799c
Brenden Coleman <bcole2433@gmail.com>
2019-08-11 09:56:30 -0400

    set up filtering by room type, capacity, and price range. Filtering TODO: filter by room size, and breakfast, pets checkboxes

--- src/Context.js
+++ src/Context.js
@@ -8,7 +8,16 @@ class RoomProvider extends Component {
     rooms: [],
     sortedRooms: [],
     featuredRooms: [],
-    loading: true
+    loading: true,
+    type: "all",
+    capacity: 1,
+    price: 0,
+    minPrice: 0,
+    maxPrice: 0,
+    minSize: 0,
+    maxSize: 0,
+    breakfast: false,
+    pets: false
   };
 
   //TODO: getData
@@ -16,11 +25,17 @@ class RoomProvider extends Component {
   componentDidMount() {
     let rooms = this.formatData(items);
     let featuredRooms = rooms.filter(room => room.featured === true);
+    let maxPrice = Math.max(...rooms.map(item => item.price));
+    let maxSize = Math.max(...rooms.map(item => item.size));
+
     this.setState({
       rooms,
       featuredRooms,
       sortedRooms: rooms,
-      loading: false
+      loading: false,
+      price: maxPrice,
+      maxPrice,
+      maxSize
     });
     console.log(rooms);
   }
@@ -36,19 +51,69 @@ class RoomProvider extends Component {
     return tempItems;
   };
 
-  getRoom = (slug) => {
+  getRoom = slug => {
     let tempRooms = [...this.state.rooms];
     const room = tempRooms.find(room => room.slug === slug);
     return room;
-  }
+  };
+
+  handleChange = e => {
+    const target = e.target;
+    const value = e.type === "checkbox" ? target.checked : target.value;
+    const name = e.target.name;
+    this.setState(
+      {
+        [name]: value
+      },
+      this.filterRooms
+    );
+  };
+
+  filterRooms = () => {
+    let {
+      rooms,
+      type,
+      capacity,
+      price,
+      minSize,
+      maxSize,
+      breakfast,
+      pets
+    } = this.state;
+
+    // ALL ROOMS
+    let tempRooms = [...rooms];
+
+    //converting values
+    capacity = parseInt(capacity);
+
+    //filter by type
+    if (type !== "all") {
+      tempRooms = tempRooms.filter(room => room.type === type);
+    }
+
+    //filter capacity
+    if (capacity !== 1) {
+      tempRooms = tempRooms.filter(room => room.capacity >= capacity);
+    }
+
+    //filter price
+    tempRooms = tempRooms.filter(room => room.price <= price);
+
+    this.setState({
+      sortedRooms: tempRooms
+    });
+  };
 
   render() {
     return (
-      <RoomContext.Provider 
-      value={{ 
-        ...this.state,
-        getRoom: this.getRoom,
-         }}>
+      <RoomContext.Provider
+        value={{
+          ...this.state,
+          getRoom: this.getRoom,
+          handleChange: this.handleChange
+        }}
+      >
         {this.props.children}
       </RoomContext.Provider>
     );
--- src/components/RoomFilter.js
+++ src/components/RoomFilter.js
@@ -1,7 +1,101 @@
 import React from "react";
+import { useContext } from "react";
+import { RoomContext } from "../Context";
+import Title from "../components/Title";
 
-const RoomFilter = () => {
-  return <div>hello from room filter</div>;
+//get all unique select values
+const getUnique = (items, value) => {
+  return [...new Set(items.map(item => item[value]))];
+};
+
+const RoomFilter = ({ rooms }) => {
+  const context = useContext(RoomContext);
+  const {
+    handleChange,
+    type,
+    capacity,
+    price,
+    minPrice,
+    maxPrice,
+    minSize,
+    maxSize,
+    breakfast,
+    pets
+  } = context;
+  // GET UNIQUE TYPES
+  let types = getUnique(rooms, "type");
+  //get all
+  types = ["all", ...types];
+  // mapping to return
+  types = types.map((item, index) => {
+    return (
+      <option value={item} key={index}>
+        {item}
+      </option>
+    );
+  });
+
+  // GET GUEST NUMBERS
+  let guests = getUnique(rooms, "capacity");
+  // mapping to return
+  guests = types.map((item, index) => {
+    return (
+      <option value={item} key={index}>
+        {item}
+      </option>
+    );
+  });
+
+  return (
+    <section className="filter-container">
+      <Title title="Search Rooms" />
+      <form className="filter-form">
+        {/*slect type */}
+        <div className="form-group">
+          <label htmlFor="type">Room Type</label>
+          <select
+            className="form-control"
+            name="type"
+            id="type"
+            value={type}
+            onChange={handleChange}
+          />
+          {types}
+        </div>
+        {/*end select type */}
+
+        {/*slect guests */}
+        <div className="form-group">
+          <label htmlFor="capacity">Guests</label>
+          <select
+            className="form-control"
+            name="capacity"
+            id="capacity"
+            value={capacity}
+            onChange={handleChange}
+          />
+          {guests}
+        </div>
+        {/*end select type */}
+
+        {/*room price */}
+        <div className="form-group">
+          <label htmlFor="price">Room Price ${price}</label>
+          <input
+            className="form-control"
+            type="range"
+            name="price"
+            min={minPrice}
+            max={maxPrice}
+            id="price"
+            value={price}
+            onChange={handleChange}
+          />
+        </div>
+        {/*end room price */}
+      </form>
+    </section>
+  );
 };
 
 export default RoomFilter;
