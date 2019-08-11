17e3520648070076c15381c3e1de5c1c55eae6f9
Brenden Coleman <bcole2433@gmail.com>
2019-08-11 11:28:24 -0400

    switched data from static data to using Contentful API. Setting up to host on Netlify

--- src/Context.js
+++ src/Context.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import items from "./data";
+// import items from "./data";
+import Client from "./Contentful";
 
 const RoomContext = React.createContext();
 
@@ -20,24 +21,36 @@ class RoomProvider extends Component {
     pets: false
   };
 
-  //TODO: getData
+//getting data from Contentful
+  getData = async () => {
+    try {
+      let response = await Client.getEntries({
+        content_type: "beachResortRoom",
+        order: "sys.type"
+      });
+
+      let rooms = this.formatData(response.items);
+      let featuredRooms = rooms.filter(room => room.featured === true);
+      let maxPrice = Math.max(...rooms.map(item => item.price));
+      let maxSize = Math.max(...rooms.map(item => item.size));
+
+      this.setState({
+        rooms,
+        featuredRooms,
+        sortedRooms: rooms,
+        loading: false,
+        price: maxPrice,
+        maxPrice,
+        maxSize
+      });
+      console.log(rooms);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   componentDidMount() {
-    let rooms = this.formatData(items);
-    let featuredRooms = rooms.filter(room => room.featured === true);
-    let maxPrice = Math.max(...rooms.map(item => item.price));
-    let maxSize = Math.max(...rooms.map(item => item.size));
-
-    this.setState({
-      rooms,
-      featuredRooms,
-      sortedRooms: rooms,
-      loading: false,
-      price: maxPrice,
-      maxPrice,
-      maxSize
-    });
-    console.log(rooms);
+    this.getData();
   }
 
   formatData = items => {
